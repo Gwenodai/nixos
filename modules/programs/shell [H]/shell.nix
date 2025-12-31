2ac910c41b6d48d0c81b2d07b41975fa7ad0f6f8
Gwen <gwenpark37@gmail.com>
2026-01-01 01:26:28 +1100

    add zsh/bash shells

---
+++ modules/programs/shell [H]/shell.nix
@@ -0,0 +1,26 @@
+{
+  flake.modules.homeManager.shell = {
+    config,
+    ...
+  }:
+
+  {
+    # Custom Aliases
+    home.shellAliases = {
+      ll = "eza -Al --icons --group-directories-first";
+      ls = "eza -A --icons --group-directories-first";
+      cat = "bat";
+    };
+
+    programs.zsh = {
+      enable = true;
+      dotDir = "${config.xdg.configHome}/zsh";
+      enableCompletion = true;
+    };
+
+    programs.bash = {
+      enable = true;
+      enableCompletion = true;
+    };
+  };
+}
