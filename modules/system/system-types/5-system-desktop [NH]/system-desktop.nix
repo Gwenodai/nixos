d78bca6956d1e7761d8aa09d20ab9ca5b1324ddb
Gwen <gwenpark37@gmail.com>
2026-01-07 16:47:45 +1100

    declared browser module in system-desktop

--- modules/system/system-types/5-system-desktop [NH]/system-desktop.nix
+++ modules/system/system-types/5-system-desktop [NH]/system-desktop.nix
@@ -9,16 +9,13 @@
   flake.modules.nixos.system-desktop = {
     imports = with inputs.self.modules.nixos; [
       system-cli
-      # TODO: Implement:
-      # printing
     ];
   };
 
   flake.modules.homeManager.system-desktop = {
     imports = with inputs.self.modules.homeManager; [
       system-cli
-      # TODO: Implement:
-      # browser
+      browser
     ];
   };
 }
