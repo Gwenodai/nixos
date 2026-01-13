f878659b5aa6b532ae95ff4ec2a5f190b18cf18d
Gwen <gwenpark37@gmail.com>
2026-01-13 18:05:51 +1100

    Define more 'cli-tools' pkgs
    - file
    - tree
    - which
    - wget
    - curl
    - gnused
    - gawk

--- modules/programs/cli-tools [NH]/pkgs [N]/pkgs.nix
+++ modules/programs/cli-tools [NH]/pkgs [N]/pkgs.nix
@@ -6,6 +6,13 @@
     environment.systemPackages = with pkgs; [
       home-manager # Manage a user environment using Nix
       parted # CLI program for creating and manipulating partition tables
+      file # Program that shows the type of files
+      tree # Command to produce a depth indented directory listing
+      which # Shows the full path of (shell) commands
+      wget # Tool for retrieving files using HTTP, HTTPS, and FTP
+      curl # Command line tool for transferring files with URL syntax
+      gnused # GNU sed, a batch stream editor
+      gawk # GNU implementation of the Awk programming language
     ];
   };
 }
