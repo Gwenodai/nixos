679b018c30a185b2ce505cf87dc0babcf059dbd3
Gwen <gwenpark37@gmail.com>
2026-01-13 17:21:55 +1100

    add bat
    **Better cat with syntax highlighting**
    Also enabled 'extraPackages':
     - batman
     - batgrep
     - batdiff
     - batpipe
     - batwatch
     - prettybat

---
+++ modules/programs/cli-tools [NH]/bat [H]/bat.nix
@@ -0,0 +1,22 @@
+{
+  # Better cat with syntax highlighting
+  flake.modules.homeManager.cli-tools = {
+    pkgs,
+    ...
+  }: {
+    programs.bat = {
+      enable = true;
+      # config = {
+      #   theme = "TwoDark";
+      # };
+      extraPackages = with pkgs.bat-extras; [
+        batman # Read system manual pages (man) using bat as the manual page formatter
+        batgrep # Quickly search through and highlight files using ripgrep
+        batdiff # Diff a file against the current git index, or display the diff between two files
+        batpipe # Less (and soon bat) preprocessor for viewing more types of files in the terminal
+        batwatch # Watch for changes in one or more files, and print them with bat
+        prettybat # Pretty-print source code
+      ];
+    };
+  };
+}
