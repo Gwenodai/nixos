507c1201190d930e4efe28990d1d6f2c6e0aaab0
Gwenodai <gwenpark37@gmail.com>
2026-03-11 18:57:01 +1100

    preservation functioning tmpfiles forwards

--- modules/core/preservation.nix
+++ modules/core/preservation.nix
@@ -1,7 +1,7 @@
 # Ephemeral state management
 # https://nix-community.github.io/preservation/configuration-options.html
 { inputs, den, lib, ... }: let
-  # System level `persist` class declare preservation config
+  # System level `persist` class for declaring preservation config
   persistClass = { class, aspect-chain }: den.provides.forward {
     each = lib.singleton true;
     fromClass = _: "persist";
@@ -11,16 +11,40 @@
     guard = { options, ... }@osArgs: options ? preservation;
     adaptArgs = args: args // { osConfig = args.config; };
   };
-  # User level `persist` class declare preservation config within home-manager
+  # User level `persist` class for declaring preservation config within home-manager
   persistUserClass = { host, user }: { class, aspect-chain }: den.provides.forward {
     each = lib.singleton user;
-    fromClass = _: "userPersist";
+    fromClass = _: "persistUser";
     intoClass = _: "nixos"; # Preservation only supports NixOS
-    intoPath = u: [ "my" "preservation" "userPersist" u.userName ];
+    intoPath = u: [ "my" "preservation" "persistUser" u.userName ];
     fromAspect = _: lib.head aspect-chain;
     guard = { options, ... }@osArgs: options ? preservation;
     # Allows access to the home-manager user config within the class
-    # via `userPersist = { hmConfig, ... }: {...};`
+    # via `persistUser = { hmConfig, ... }: {...};`
+    adaptArgs = { config, ... }@args: args // {
+      hmConfig = config.home-manager.users.${user.userName};
+    };
+  };
+
+  # System level `tmpfiles` class for declaring `systemd.tmpfiles` cleanly
+  persistTmpClass = { class, aspect-chain }: den.provides.forward {
+    each = lib.singleton true;
+    fromClass = _: "persistTmp";
+    intoClass = _: "nixos";
+    intoPath = _: [ "my" "preservation" "tmpfiles" ];
+    fromAspect = _: lib.head aspect-chain;
+    guard = { options, ... }@osArgs: options ? preservation;
+    adaptArgs = args: args // { osConfig = args.config; };
+  };
+
+  # User level `tmpfiles` class for declaring `systemd.tmpfiles` config within home-manager
+  persistUserTmpClass = { host, user }: { class, aspect-chain }: den.provides.forward {
+    each = lib.singleton user;
+    fromClass = _: "persistUserTmp";
+    intoClass = _: "nixos";
+    intoPath = u: [ "my" "preservation" "userTmpfiles" u.userName ];
+    fromAspect = _: lib.head aspect-chain;
+    guard = { options, ... }@osArgs: options ? preservation;
     adaptArgs = { config, ... }@args: args // {
       hmConfig = config.home-manager.users.${user.userName};
     };
@@ -32,23 +56,49 @@ in {
   };
 
   # TODO: Move to `den.ctx.hm-user.includes` after `hm-user` is fixed upstream
-  den.ctx.user.includes = [ persistUserClass ]; # Register the `userPersist` class
+  den.ctx.user.includes = [ persistUserClass persistUserTmpClass ]; # Register the `persistUser` class
 
   den.aspects.persist = {
-    includes = [ persistClass ]; # Register the `persist` class  
+    includes = [ persistClass persistTmpClass ]; # Register the `persist` class  
     nixos = { config, lib, ... }: {
       # Import the preservation module
       imports = [ inputs.preservation.nixosModules.preservation ];
 
-      options.my.preservation.userPersist = lib.mkOption {
-        type = lib.types.attrsOf (lib.types.attrsOf lib.types.anything);
-        default = {};
-        description = "Intermediate holding ground for userPersist configurations.";
+      options.my.preservation = {
+        persistUser = lib.mkOption {
+          type = lib.types.attrsOf (lib.types.attrsOf lib.types.anything);
+          default = {};
+          description = "Intermediate holding ground for persistUser configurations.";
+        };
+        tmpfiles = lib.mkOption {
+          type = lib.types.attrsOf lib.types.anything;
+          default = {};
+        };
+        userTmpfiles = lib.mkOption {
+          type = lib.types.attrsOf (lib.types.attrsOf lib.types.anything);
+          default = {};
+        };
       };
       config = {
         preservation.enable = true; # Enable the module
 
-        # Intercept and transform the output of `userPersist` before it reaches `preservation`
+        /* Intercept and transform the output of `persistUser` before it reaches `preservation`
+          It takes:
+            persistUser = { hmConfig, ... }: {
+              directories = [
+                "${hmConfig.xdg.configHome}/sops"
+                { directory = "userdir/subdir"; mode = "0600"; }
+              ];
+            };
+          And transforms it to:
+            preservation.preserveAt."/persist".users.<user> = {
+              directories = [
+                ".config/sops"
+                { directory = "userdir/subdir"; mode = "0600"; }
+              ];
+            };
+          "${hmConfig.xdg.configHome}" would normally become "/home/<user>/.config" which preservation
+          would translate into "/home/<user>/home/<user>/.config" as it expects relative paths */
         preservation.preserveAt."/persist".users = lib.mapAttrs (userName: rawConfig:
           let # `homePrefix` evaluates to "/home/<user>/" (usually)
             homePrefix = "${config.home-manager.users.${userName}.home.homeDirectory}/";
@@ -73,7 +123,46 @@ in {
                 strip val; # Use `strip` to sanitise value for the preservation module
           in
             transform rawConfig # `rawConfig` = `{ directories = [...]; files = [...]; }`
-        ) config.my.preservation.userPersist; # Intermediate `userPersist` config location
+        ) config.my.preservation.persistUser; # Intermediate `persistUser` config location
+        
+        /* Intercept and transform the output of `persistTmp` and `persistUserTmp`
+          before it reaches `systemd.tmpfiles.settings.preservation`
+          It takes:
+            "/dir/subdir" = { };
+            "relative/user/dir" = { };
+            "${hmConfig.xdg.configHome}/foo" = { mode = "0600"; };
+          And transforms it to:
+            "/dir/subdir".d = { user = "root"; group = "root"; mode = "0755"; };
+            "/home/<user>/relative/user/dir".d = { user = "<user>"; group = "users"; mode = "755"; };
+            "/home/<user>/.config/foo".d = { user = "<user>"; group = "users"; mode = "0600"; }; */
+        systemd.tmpfiles.settings.preservation = lib.mkMerge [
+          # System level tmpfiles config (aka `persistTmp`)
+          (lib.mapAttrs' (path: opts: 
+            lib.nameValuePair path { # Set default tmpfiles options
+              d = { user = "root"; group = "root"; mode = "0755"; } // opts; # Merge supplied opts
+            }
+          ) config.my.preservation.tmpfiles) # Intermediate `tmpfiles` config location
+          # User level tmpfiles config (aka `persistUserTmp`)
+          (lib.mkMerge (lib.mapAttrsToList (userName: rawConfig:
+            let
+              homeDir = config.home-manager.users.${userName}.home.homeDirectory;
+              # Fallback to "users" group if the user's primary group isn't defined
+              group = config.users.users.${userName}.group or "users";
+              # Convert relative paths to absolute paths
+              makeAbsolute = path: 
+                if lib.hasPrefix "/" path then # Prepend home directory only if the
+                  path                         # path is relative (doesn't start with "/")
+                else
+                  "${homeDir}/${path}";
+            in
+              lib.mapAttrs' (path: opts:
+                lib.nameValuePair (makeAbsolute path) {
+                  # Derive and set default tmpfiles options
+                  d = { user = userName; inherit group; mode = "0755"; } // opts; # Merge supplied opts
+                }
+              ) rawConfig # `rawConfig` = `"/dir/subdir" = { }; "/foo/bar" = { mode = "0600"; };`
+          ) config.my.preservation.userTmpfiles)) # Intermediate `userTmpfiles` config location
+        ];
       };
     };
   };
@@ -96,7 +185,7 @@ in {
       ];
     };
 
-    userPersist = { hmConfig, ... }: {
+    persistUser = { hmConfig, ... }: {
       directories = [
         { directory = "userdir/foo"; mode = "0600"; }
       ];
@@ -104,6 +193,18 @@ in {
         "userfile/bar.file"
         "${hmConfig.xdg.configHome}/sops"
       ];
+    };
+
+    persistTmp = {
+      "/foo/bar/dir" = { };
+    };
+
+    persistUserTmp = { hmConfig, ... }: {
+      "${hmConfig.xdg.configHome}/foo/bar" = { mode = "0700"; };
+      "${hmConfig.xdg.configHome}/foo" = { mode = "0700"; };
+      "${hmConfig.xdg.configHome}" = { };
+      
+      "Documents/PersistTest" = { mode = "0700"; }; 
     };
   };
   den.aspects.gwen-t1.includes = [ den.aspects.persist ]; # Host
