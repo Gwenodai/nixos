655a26b3df840ced2e44e49ba3d9ef04a076c4c9
Gwenodai <gwenpark37@gmail.com>
2026-03-12 20:00:19 +1100

    ssh ported to den

---
+++ modules/services/ssh.nix
@@ -0,0 +1,46 @@
+{ den, ... }: {
+  den.ctx.host.includes = [ den.aspects.ssh ];
+
+  den.aspects.ssh = {
+    includes = with den.aspects.ssh.provides; [
+      openssh
+      persist
+    ];
+
+    provides.openssh = {
+      nixos = { lib, ... }: {
+        services.openssh = {
+          enable = lib.mkDefault true;
+          openFirewall = lib.mkDefault true;
+          settings = {
+            PermitRootLogin = lib.mkDefault "no";
+            PasswordAuthentication = lib.mkDefault true;
+          };
+        };
+      };
+    };
+    
+    provides.persist = {
+      persist.files = map (path: {
+        file = path;
+        how = "symlink";
+        inInitrd = true; # Needed for `sops-nix` user password
+        configureParent = true;
+      }) [
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
+        "/etc/ssh/ssh_host_rsa_key"
+        "/etc/ssh/ssh_host_rsa_key.pub"
+      ];
+
+      persistUser.directories = [
+        {
+          directory = ".ssh";
+          how = "symlink";
+          mode = "0700";
+          createLinkTarget = true;
+        }
+      ];
+    };
+  };
+}
