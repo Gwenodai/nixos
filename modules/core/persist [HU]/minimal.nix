Gwenodai/nixos

commit e670975b493056beb0067283349b51ef7cdb2038

add dconf-editor (for debug purposes)

modules/core/persist [HU]/minimal.nix
@@ -84,10 +84,12 @@
         directories = [
           "dots" # Nix flake directory
           { directory = ".pki"; mode = "0700"; }
-          "${hmConfig.xdg.configHome}/dconf"
           "${hmConfig.xdg.dataHome}/systemd/timers"
           "${hmConfig.xdg.cacheHome}/gtk-4.0/vulkan-pipeline-cache"
           "${hmConfig.xdg.cacheHome}/qtshadercache-x86_64-little_endian-lp64"
+        ];
+        files = [
+          { file = "${hmConfig.xdg.configHome}/dconf/user"; mode = "0644"; }
         ];
       };
 

modules/hosts/gwen-t1/configuration.nix
@@ -37,8 +37,9 @@
     };
 
     _.gwen = {
-      includes = [
+      includes = with den.aspects; [
         den._.primary-user
+        dconf-editor
       ];
     };
     

modules/programs/dconf-editor [U]/dconf-editor.nix
@@ -1,0 +1,17 @@
+{ den, ... }: {
+  den.aspects.dconf-editor = {
+    includes = with den.aspects.dconf-editor._; [ enable ];
+    
+    _.enable = den.lib.perUser {
+      homeManager = { pkgs, lib, ... }: {
+        home.packages = with pkgs; [
+          dconf-editor # Tool for easily viewing dconf settings
+        ];
+
+        dconf.settings."ca/desrt/dconf-editor" = lib.mkDefault {
+          show-warning = false;
+        };
+      };
+    };
+  };
+}

commit 39271e126969f01cf4b9c835174d80c3aa1c970b

fix(persist): I'm a dumbass. e670975b493056beb0067283349b51ef7cdb2038 broke dconf persist changes made in 2347cd7cf9de482ea06ee8a3e75db6b5ee743c94
Sleepy me already figured out the correct way to persist dconf 2 days ago.
dconf wants to rename the `"~/.config/dconf/user"` file when it makes changes. So persisting the exact file causes issues. (Just persist the dir instead)

modules/core/persist [HU]/minimal.nix
@@ -84,12 +84,10 @@
         directories = [
           "dots" # Nix flake directory
           { directory = ".pki"; mode = "0700"; }
+          "${hmConfig.xdg.configHome}/dconf"
           "${hmConfig.xdg.dataHome}/systemd/timers"
           "${hmConfig.xdg.cacheHome}/gtk-4.0/vulkan-pipeline-cache"
           "${hmConfig.xdg.cacheHome}/qtshadercache-x86_64-little_endian-lp64"
-        ];
-        files = [
-          { file = "${hmConfig.xdg.configHome}/dconf/user"; mode = "0644"; }
         ];
       };
 