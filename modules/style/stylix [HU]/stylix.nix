f898ba9937bd20d146620855620353a8b4615ab7
Gwenodai <gwenpark37@gmail.com>
2026-03-25 12:49:38 +1100

    improve(display-manager): new sub-aspects `gdm`, `greetd`. `gdm` is now the default display-manager.

--- modules/style/stylix [HU]/stylix.nix
+++ modules/style/stylix [HU]/stylix.nix
@@ -8,10 +8,12 @@
     includes = with den.aspects.stylix._; [ sys user ];
 
     _.sys = den.lib.perHost {
-      nixos = { pkgs, lib, ... }: {
+      nixos = {  config, pkgs, lib, ... }: {
         imports = [ inputs.stylix.nixosModules.stylix ];
         stylix = {
           enable = lib.mkDefault true;
+          homeManagerIntegration.autoImport = lib.mkDefault true;
+          homeManagerIntegration.followSystem = lib.mkDefault true;
           autoEnable = lib.mkDefault false;
           polarity = "dark";
           base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/pinky.yaml";
@@ -63,6 +65,8 @@
           targets = {
             gtk.enable = lib.mkDefault true;
             qt.enable = lib.mkDefault true;
+            gnome.enable = lib.mkDefault config.services.displayManager.gdm.enable;
+            regreet.enable = lib.mkDefault config.programs.regreet.enable;
           };
         };
       };
@@ -70,21 +74,23 @@
 
     _.user = den.lib.perUser {
       homeManager = { config, lib, ... }: {
-        stylix.targets = {
-          gtk = lib.mkDefault {
-            enable = true;
-            flatpakSupport.enable = true;
-            # Force dialog buttons to use white text
-            extraCss = ''
-              dialog button, .dialog-action-area > .text-button {
-                color: @theme_text_color; 
-              }
-            '';
-          };
+        stylix = {
+          targets = {
+            gtk = lib.mkDefault {
+              enable = true;
+              flatpakSupport.enable = true;
+              # Force dialog buttons to use white text
+              extraCss = ''
+                dialog button, .dialog-action-area > .text-button {
+                  color: @theme_text_color; 
+                }
+              '';
+            };
 
-          kitty.enable = lib.mkDefault true;
-          btop.enable = lib.mkDefault true;
-          qt.enable = lib.mkDefault true;
+            kitty.enable = lib.mkDefault config.programs.kitty.enable;
+            btop.enable = lib.mkDefault config.programs.btop.enable;
+            qt.enable = lib.mkDefault true;
+          };
         };
         
         dconf.settings."org/gnome/desktop/interface" = {
--- modules/system/system-presets/desktop-type [HU]/desktop-type.nix
+++ modules/system/system-presets/desktop-type [HU]/desktop-type.nix
@@ -2,11 +2,16 @@
 # These should be imported by BOTH hosts and users wanting to use them.
 { den, ... }: {
   den.aspects.desktop-type = {
-    _.desktop-environment = {};
+    _.desktop-environment = {}; # None defined yet
 
     _.window-manager = {
       _.niri = {
         includes = with den.aspects; [
+          ( den.lib.perHost {
+            nixos = { lib, ... }: { # Set the default login session to Niri
+              services.displayManager.defaultSession = lib.mkDefault "niri";
+            };
+          })
           # ---Core Aspects--- #
           niri
           noctalia # Minimal desktop shell
