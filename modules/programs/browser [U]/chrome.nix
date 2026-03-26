Gwenodai/nixos

commit 6e793579dffad1f229c5d9bd65d13701eee0ff6a

clean up `programs`
Just a few changes for organisation I made while writing up the programs wiki

modules/programs/browser [U]/chrome.nix
@@ -3,70 +3,66 @@
     # The default sub-aspect included when the generic 'browser' aspect is used
     includes = with den.aspects.browser._; [ chrome ];
     
-    _.chrome = {
-      includes = with den.aspects.browser._.chrome._; [ enable ];
+    _.chrome = den.lib.perHost {
+      homeManager = { pkgs, lib, ... }: {
+        home.packages = with pkgs; [
+          google-chrome
+        ];
 
-      _.enable = den.lib.perHost {
-        homeManager = { pkgs, lib, ... }: {
-          home.packages = with pkgs; [
-            google-chrome
-          ];
-
-          xdg = {
-            mimeApps = {
-              defaultApplications = lib.mkBefore (
-                let
-                  application = "google-chrome.desktop";
-                  mimeTypes = [
-                    "application/rdf+xml"
-                    "application/rss+xml"
-                    "application/xhtml+xml"
-                    "application/xhtml_xml"
-                    "x-scheme-handler/http"
-                    "x-scheme-handler/https"
-                    "x-scheme-handler/about"
-                    "x-scheme-handler/mailto"
-                    "x-scheme-handler/unknown"
-                    "x-scheme-handler/google-chrome"
-                  ];
-                in lib.genAttrs mimeTypes (mimetype: application)
-              );
-              associations.added = let
+        xdg = {
+          mimeApps = {
+            defaultApplications = lib.mkBefore (
+              let
                 application = "google-chrome.desktop";
                 mimeTypes = [
-                  "application/xml"
-                  "application/pdf"
-                  "text/markdown"
-                  "image/jpeg"
-                  "image/webp"
-                  "image/gif"
-                  "image/png"
-                  "text/html"
-                  "text/xml"
+                  "application/rdf+xml"
+                  "application/rss+xml"
+                  "application/xhtml+xml"
+                  "application/xhtml_xml"
+                  "x-scheme-handler/http"
+                  "x-scheme-handler/https"
+                  "x-scheme-handler/about"
+                  "x-scheme-handler/mailto"
+                  "x-scheme-handler/unknown"
+                  "x-scheme-handler/google-chrome"
                 ];
-              in lib.genAttrs mimeTypes (mimetype: application);
-            };
+              in lib.genAttrs mimeTypes (mimetype: application)
+            );
+            associations.added = let
+              application = "google-chrome.desktop";
+              mimeTypes = [
+                "application/xml"
+                "application/pdf"
+                "text/markdown"
+                "image/jpeg"
+                "image/webp"
+                "image/gif"
+                "image/png"
+                "text/html"
+                "text/xml"
+              ];
+            in lib.genAttrs mimeTypes (mimetype: application);
           };
         };
+      };
 
-        persistUser = { hmConfig, ... }: {
-          directories = [
-            {
-              directory = "${hmConfig.xdg.configHome}/google-chrome";
-              how = "symlink";
-              mode = "0700";
-              createLinkTarget = true;
-            }
-          ];
-        };
+      persistUser = { hmConfig, ... }: {
+        directories = [
+          {
+            directory = "${hmConfig.xdg.configHome}/google-chrome";
+            how = "symlink";
+            mode = "0700";
+            createLinkTarget = true;
+          }
+        ];
+      };
 
-        persistUserTmp = { hmConfig, ... }: {
-          "${hmConfig.xdg.configHome}" = {}; # "~/.config"
-        };
+      persistUserTmp = { hmConfig, ... }: {
+        "${hmConfig.xdg.configHome}" = {}; # "~/.config"
+      };
 
-        persistUserIgnore = { hmConfig, ... }: {
-          directories = [ "${hmConfig.xdg.cacheHome}/google-chrome" ];
-        };
+      persistUserIgnore = { hmConfig, ... }: {
+        directories = [ "${hmConfig.xdg.cacheHome}/google-chrome" ];
       };
     };
   };

modules/programs/git [U]/git.nix
@@ -1,20 +1,34 @@
 { den, lib, ... }: {
   den.aspects.git = {
-    includes = with den.aspects.git._; [
-      git
-      gh
-      gitClass
-    ];
+    # All sub-aspects are included when the generic 'git' aspect is used
+    includes = lib.attrValues den.aspects.git._;
     
-    _.git = den.lib.perUser {
-      homeManager = { lib, ... }: {
-        programs.git = {
-          enable = lib.mkDefault true;
-          settings = {
-            init.defaultBranch = lib.mkDefault  "main";
+    _.git = {
+      # Bundles all git components when the complete 'git' sub-aspect is used
+      includes = lib.attrValues den.aspects.git._.git._;
+
+      enable = den.lib.perUser {
+        homeManager = { lib, ... }: {
+          programs.git = {
+            enable = lib.mkDefault true;
+            settings = {
+              init.defaultBranch = lib.mkDefault  "main";
+            };
           };
         };
       };
+
+      _.class = den.lib.perUser (
+        { class, aspect-chain }: den._.forward {
+          each = lib.singleton true;
+          fromClass = _: "git";
+          intoClass = _: "homeManager";
+          intoPath = _: [ "programs" "git" ];
+          fromAspect = _: lib.head aspect-chain;
+          adaptArgs = lib.id;
+          guard = { config, ... }@hmArgs: _: lib.mkIf config.programs.git.enable;
+        }
+      );
     };
 
     _.gh = den.lib.perUser {
@@ -27,17 +41,5 @@
         };
       };
     };
-
-    _.gitClass = den.lib.perUser (
-      { class, aspect-chain }: den._.forward {
-        each = lib.singleton true;
-        fromClass = _: "git";
-        intoClass = _: "homeManager";
-        intoPath = _: [ "programs" "git" ];
-        fromAspect = _: lib.head aspect-chain;
-        adaptArgs = lib.id;
-        guard = { config, ... }@hmArgs: _: lib.mkIf config.programs.git.enable;
-      }
-    );
   };
 }

modules/programs/vscode [U]/!includes.nix
@@ -3,23 +3,7 @@
     enable
     settings
     languages
-    
-    # ---Extensions--- #
-    extensions._.todo-highlight
-    extensions._.path-intellisense
-
-    # ---Themes--- #
-    themes._.colours._.monokai-vibrant
-    themes._.icons._.symbols
-    # Theme config
-    ( den.lib.perUser {
-      homeManager = { lib, ... }: {
-        programs.vscode.profiles.default.userSettings.workbench = {
-          # DON'T FORGET TO SET DEFAULT THEME SETTINGS HERE
-          colorTheme = lib.mkDefault "Monokai Vibrant";
-          iconTheme = lib.mkDefault "symbols";
-        };
-      };
-    })
+    extensions
+    themes._.default
   ];
 }

modules/programs/vscode [U]/themes/themes.nix
@@ -3,7 +3,7 @@
     _.colours = {
   # ---Colours--- #
       _.monokai-vibrant = den.lib.perUser {
-        includes = [ den.aspects.vscode._.extensions ];
+        includes = [ den.aspects.vscode._.extensions._.enable ];
         homeManager = { pkgs, lib, ... }: {
           programs.vscode.profiles.default = {
             extensions = with pkgs.nix-vscode-extensions.vscode-marketplace-release; [
@@ -17,7 +17,7 @@
     _.icons = {
   # ---Icons--- #
       _.catppuccin-vsc-icons = den.lib.perUser {
-        includes = [ den.aspects.vscode._.extensions ];
+        includes = [ den.aspects.vscode._.extensions._.enable ];
         homeManager = { pkgs, lib, ... }: {
           programs.vscode.profiles.default = {
             extensions = with pkgs.nix-vscode-extensions.vscode-marketplace-release; [
@@ -30,7 +30,7 @@
       };
 
       _.symbols = den.lib.perUser {
-        includes = [ den.aspects.vscode._.extensions ];
+        includes = [ den.aspects.vscode._.extensions._.enable ];
         homeManager = { pkgs, lib, ... }: {
           programs.vscode.profiles.default = {
             extensions = with pkgs.nix-vscode-extensions.vscode-marketplace-release; [

modules/programs/files [U]/nemo/!includes.nix
@@ -1,9 +1,6 @@
-{ den, ... }: {
+{ den, lib, ... }: {
   den.aspects.files = {
     # Bundles all nemo components when the complete 'nemo' sub-aspect is used
-    includes = with den.aspects.files._.nemo._; [
-      enable
-      config
-    ];
+    includes = lib.attrValues den.aspects.files._.nemo._;
   };
 }

modules/programs/messaging [U]/!includes.nix
@@ -1,0 +1,6 @@
+{ den, lib, ... }: {
+  den.aspects.messaging = {
+    # All default messenging programs are included when the generic 'messaging' aspect is used
+    includes = lib.attrValues den.aspects.messaging._;
+  };
+}

modules/programs/messaging [U]/discord/vesktop/!includes.nix
@@ -1,9 +1,6 @@
-{ den, ... }: {
+{ den, lib, ... }: {
   den.aspects.messaging._.discord._.vesktop = {
     # Bundles all vesktop components when the complete 'vesktop' sub-aspect is used
-    includes = with den.aspects.messaging._.discord._.vesktop._; [
-      enable
-      config
-    ];
+    includes = lib.attrValues den.aspects.messaging._.discord._.vesktop._;
   };
 }

modules/programs/shell [U]/!includes.nix
@@ -1,7 +1,6 @@
-{ den, ... }: {
-  den.aspects.shell.includes = with den.aspects.shell._; [
-    zsh
-    bash
-    starship
-  ];
+{ den, lib, ... }: {
+  den.aspects.shell = {
+    # All sub-aspects are included when the generic 'shell' aspect is used
+    includes = lib.attrValues den.aspects.shell._;
+  };
 }

commit fba49b533a6386f3498adad8c5be7626271d1800

fix(bugs introduced in 6e793579dffad1f229c5d9bd65d13701eee0ff6a)
Not sure why I can't properly include vscode themes so I've reverted that for now

modules/programs/browser [U]/chrome.nix
@@ -3,7 +3,7 @@
     # The default sub-aspect included when the generic 'browser' aspect is used
     includes = with den.aspects.browser._; [ chrome ];
     
-    _.chrome = den.lib.perHost {
+    _.chrome = den.lib.perUser {
       homeManager = { pkgs, lib, ... }: {
         home.packages = with pkgs; [
           google-chrome

modules/programs/git [U]/git.nix
@@ -7,7 +7,7 @@
       # Bundles all git components when the complete 'git' sub-aspect is used
       includes = lib.attrValues den.aspects.git._.git._;
 
-      enable = den.lib.perUser {
+      _.enable = den.lib.perUser {
         homeManager = { lib, ... }: {
           programs.git = {
             enable = lib.mkDefault true;

modules/programs/vscode [U]/!includes.nix
@@ -4,6 +4,18 @@
     settings
     languages
     extensions
-    themes._.default
+    # ---Themes--- #
+    themes._.colours._.monokai-vibrant
+    themes._.icons._.symbols
+    # Theme config
+    ( den.lib.perUser {
+      homeManager = { lib, ... }: {
+        programs.vscode.profiles.default.userSettings.workbench = {
+          # DON'T FORGET TO SET DEFAULT THEME SETTINGS HERE
+          colorTheme = lib.mkDefault "Monokai Vibrant";
+          iconTheme = lib.mkDefault "symbols";
+        };
+      };
+    })
   ];
 }

modules/programs/vscode [U]/themes/themes.nix
@@ -4,7 +4,7 @@
   # ---Colours--- #
       _.monokai-vibrant = den.lib.perUser {
         includes = [ den.aspects.vscode._.extensions._.enable ];
-        homeManager = { pkgs, lib, ... }: {
+        homeManager = { pkgs, ... }: {
           programs.vscode.profiles.default = {
             extensions = with pkgs.nix-vscode-extensions.vscode-marketplace-release; [
               s3gf4ult.monokai-vibrant
@@ -18,7 +18,7 @@
   # ---Icons--- #
       _.catppuccin-vsc-icons = den.lib.perUser {
         includes = [ den.aspects.vscode._.extensions._.enable ];
-        homeManager = { pkgs, lib, ... }: {
+        homeManager = { pkgs, ... }: {
           programs.vscode.profiles.default = {
             extensions = with pkgs.nix-vscode-extensions.vscode-marketplace-release; [
               catppuccin.catppuccin-vsc-icons
@@ -31,7 +31,7 @@
 
       _.symbols = den.lib.perUser {
         includes = [ den.aspects.vscode._.extensions._.enable ];
-        homeManager = { pkgs, lib, ... }: {
+        homeManager = { pkgs, ... }: {
           programs.vscode.profiles.default = {
             extensions = with pkgs.nix-vscode-extensions.vscode-marketplace-release; [
               miguelsolorio.symbols