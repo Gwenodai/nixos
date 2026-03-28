1bba21a2920f1ebef38462064e1017c844ebc00a
Gwenodai <gwenpark37@gmail.com>
2026-03-29 09:02:59 +1100

    improve(vscode): swap to better markdown extension + config to match github wiki

--- modules/programs/vscode [U]/languages/markdown.nix
+++ modules/programs/vscode [U]/languages/markdown.nix
@@ -1,18 +1,30 @@
-{ den, ... }: {
+{ inputs, den, ... }: {
   den.aspects.vscode._.languages._.markdown = den.lib.perUser {
     includes = [ den.aspects.vscode._.extensions._.enable ];
     homeManager = { pkgs, ... }: {
       programs.vscode.profiles.default = {
         extensions = with pkgs.nix-vscode-extensions.vscode-marketplace-release; [
-          # Changes VS Code's built-in markdown preview to match GitHub's styling
-          bierner.markdown-preview-github-styles
-          # Adds checkbox support to the built-in markdown preview
-          bierner.markdown-checkbox
+          # Massively improves VS Code's built-in markdown preview
+          shd101wyy.markdown-preview-enhanced
           # Adds :emoji: syntax support to VS Code's built-in Markdown preview
           bierner.markdown-emoji
-          # Adds Mermaid diagram and flowchart support to VS Code's builtin markdown preview
-          bierner.markdown-mermaid
+
+          # # Changes VS Code's built-in markdown preview to match GitHub's styling
+          # bierner.markdown-preview-github-styles
+          # # Adds Mermaid diagram and flowchart support to VS Code's builtin markdown preview
+          # bierner.markdown-mermaid
+          # # Adds checkbox support to the built-in markdown preview
+          # bierner.markdown-checkbox
         ];
+
+        userSettings.markdown-preview-enhanced = inputs.self.lib.applyDefaults {
+          previewColorScheme = "systemColorScheme";
+          previewTheme = "atom-dark.css";
+          useGitHubStylePipedLink = true;
+          mermaidTheme = "dark";
+          codeBlockTheme = "monokai.css";
+          breakOnSingleNewLine = false;
+        };
       };
     };
   };
