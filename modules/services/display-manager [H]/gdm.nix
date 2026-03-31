f23749b6f96371c5c6bc43b7a7b58d5bb39e8b4c
Gwenodai <gwenpark37@gmail.com>
2026-03-31 16:25:22 +1100

    clean up `display-manager`

--- modules/services/display-manager [H]/gdm.nix
+++ modules/services/display-manager [H]/gdm.nix
@@ -1,22 +1,18 @@
 { den, ... }: {
-  den.aspects.display-manager._.gdm = {
-    includes = with den.aspects.display-manager._.gdm._; [ enable ];
-
-    _.enable = den.lib.perHost {
-      nixos = { lib, ... }: {
-        services.displayManager = {
+  den.aspects.display-manager._.gdm = den.lib.perHost {
+    nixos = { lib, ... }: {
+      services.displayManager = {
+        enable = lib.mkDefault true;
+        gdm = {
           enable = lib.mkDefault true;
-          gdm = {
-            enable = lib.mkDefault true;
-            wayland = lib.mkDefault true;
-            autoSuspend = lib.mkDefault true;
-          };
+          wayland = lib.mkDefault true;
+          autoSuspend = lib.mkDefault true;
         };
       };
-
-      persist.directories = [
-        "/var/lib/gdm"
-      ];
     };
+
+    persist.directories = [
+      "/var/lib/gdm"
+    ];
   };
 }
--- modules/services/display-manager [H]/greetd.nix
+++ modules/services/display-manager [H]/greetd.nix
@@ -1,15 +1,11 @@
 { den, ... }: {
-  den.aspects.display-manager._.greetd = {
-    includes = with den.aspects.display-manager._.greetd._; [ enable ];
-
-    _.enable = den.lib.perHost {
-      nixos = { pkgs, lib, ... }: {
-        services.greetd = {
-          enable = lib.mkDefault true;
-          settings.default_session = {
-            command = lib.mkDefault "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --asterisks --sessions /run/current-system/sw/share/wayland-sessions";
-            user = lib.mkDefault "greeter";
-          };
+  den.aspects.display-manager._.greetd = den.lib.perHost {
+    nixos = { pkgs, lib, ... }: {
+      services.greetd = {
+        enable = lib.mkDefault true;
+        settings.default_session = {
+          command = lib.mkDefault "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --asterisks --sessions /run/current-system/sw/share/wayland-sessions";
+          user = lib.mkDefault "greeter";
         };
       };
     };
