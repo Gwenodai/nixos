258c75a58bb5a806f8735749322adb9582a1c37c
Gwen <gwenpark37@gmail.com>
2026-05-19 20:40:39 +1000

    fix: ssh config regression (#13)

--- modules/aspects/window-managers/niri [HU]/provides/config/rules/vrr.nix
+++ modules/aspects/window-managers/niri [HU]/provides/config/rules/vrr.nix
@@ -10,6 +10,7 @@ let
               { app-id = "^steam_app.*"; } # Most steam games
               { app-id = "^RSG-Linux-Shipping$"; } # Everspace native linux version
               { app-id = "^vampire crawlers.exe$"; }
+              # { app-id = "^riftbreaker_win_release.exe$"; }
             ];
             variable-refresh-rate = true;
             open-focused = true;
@@ -25,6 +26,7 @@ let
                 app-id = "^steam_app.*";
                 title = "^Dome Keeper$";
               }
+              { app-id = "^re5dx9.exe$"; } # RE:5
               { app-id = "^steam_app_881100$"; } # Noita
               { app-id = "^steam_app_323190$"; } # Frostpunk
               # { app-id = "^steam_app_2246340$"; } # MH:Wilds
