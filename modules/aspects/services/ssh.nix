1db4460107179d29ed1e4eba808d7ae591f74796
Gwen <gwenpark37@gmail.com>
2026-06-12 02:52:37 +1000

    fixed missing `/etc/ssh/` keys
    I have no idea when those went missing...

--- modules/aspects/services/ssh.nix
+++ modules/aspects/services/ssh.nix
@@ -38,6 +38,25 @@
       };
 
       ### Persist config
+      persist =
+        { lib, ... }:
+        {
+          files =
+            lib.map
+              (path: {
+                file = path;
+                how = "symlink";
+                inInitrd = true; # Needed for `sops-nix`
+                configureParent = true;
+              })
+              [
+                "/etc/ssh/ssh_host_ed25519_key"
+                "/etc/ssh/ssh_host_ed25519_key.pub"
+                "/etc/ssh/ssh_host_rsa_key"
+                "/etc/ssh/ssh_host_rsa_key.pub"
+              ];
+        };
+
       persistUser = {
         directories = [
           # "~/.ssh"
